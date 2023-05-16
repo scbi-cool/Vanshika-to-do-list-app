33052408386e4f74f8912c6b9a1cce849d28c49b
scbi-cool <kandalavanshika@gmail.com>
2023-05-16 11:22:39 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
-
+import TodoList from './TodoList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoList />
   </React.StrictMode>
 );
