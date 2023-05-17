fb369013ce4b7ebd3f4fd69635d84ef94dfaab67
scbi-cool <kandalavanshika@gmail.com>
2023-05-17 09:53:15 +0530

    yes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Todo from './Todo';
+import NoPage from './NoPage';
 import TodoList from './TodoList';
 
+
+export default function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<TodoList />}>
+                <Route index element= {<TodoList />} />
+                <Route path="/todo" element={<Todo />} />
+                <Route path="*" element={<NoPage />} />
+            </Route>        
+        </Routes>
+    </BrowserRouter>
+  );
+}
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <TodoList />
-  </React.StrictMode>
-);
+root.render(<App/>);
